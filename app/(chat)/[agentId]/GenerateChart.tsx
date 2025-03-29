0ad579c5de86332eb676fa59718002edd706e372
flashcode <laughingliangcl@gmail.com>
2025-03-30 00:41:22 +0800

    feat: new route

---
+++ app/(chat)/[agentId]/GenerateChart.tsx
@@ -0,0 +1,35 @@
+"use client";
+import { generateUUID } from "@/lib/utils";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+export  function GenerateChart({agentId}: {agentId: string}) {
+  const router = useRouter();
+  const fetchData = ({agentId}: {agentId: string}) => {
+    const id = generateUUID();
+    fetch("/api/chat/create", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        id: `${id}-${agentId}`,
+        title: agentId, // 使用代理名称作为初始标题
+        agentId: agentId,
+      }),
+    }).then(res => res.text()).then(data => {
+      console.log('data--',data);
+      router.push(`/${id}-${agentId}`);
+    }) .catch(err => {
+      console.log(err);
+    });
+  };
+  useEffect(() => {
+    console.log("GenerateChart");
+    fetchData({ agentId});
+  });
+  return (
+    <div>
+      <h1>Generating...</h1>
+    </div>
+  );
+}
